871e5a537007fee81518747eeef22a3aea8d85ad
xryuseix <ryusei143.shootingstar@gmail.com>
2021-04-13 18:29:10 +0900

    学習結果をコメントに追加

--- inferModel/model.py
+++ inferModel/model.py
@@ -8,7 +8,21 @@ def logisticRegression(trainX, testX, trainY, testY):
     lr = LogisticRegression(max_iter=300)
     lr.fit(trainX, trainY)
     return lr
+"""
+Training Accuracy : 0.970163273132383
+Testing Accuracy : 0.9594209863328892
 
+CLASSIFICATION REPORT
+
+              precision    recall  f1-score   support
+
+         Bad       0.97      0.89      0.93     38937
+        Good       0.96      0.99      0.97     98400
+
+    accuracy                           0.96    137337
+   macro avg       0.96      0.94      0.95    137337
+weighted avg       0.96      0.96      0.96    137337
+"""
 
 # 学習結果を評価
 def evaluation(model, trainX, testX, trainY, testY, name=""):
