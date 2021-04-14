87388ff0cc7ee73525a3fb62d4117c483901df24
xryuseix <ryusei143.shootingstar@gmail.com>
2021-04-14 14:08:56 +0900

    feat: 英単語辞書読み込みクラス作成

--- inferModel/infer.py
+++ inferModel/infer.py
@@ -1,19 +1,36 @@
-import joblib
+import joblib, json
 import pandas as pd
 import processing as proc
 
+# 英単語辞書管理クラス
+class wordStr2IntVec:
+    def __init__(self):
+        with open("vocabulary.json") as f:
+            self.vocabulary = json.load(f)
 
-def infer(url: str):
+    # 英単語を数字に変換
+    def convert(self, normalizedUrl: str):
+        urlList = normalizedUrl.split(" ")
+        convertedList = [self.vocabulary[word] for word in urlList]
+        return convertedList
+
+
+def infer(url: str, w2iClass):
     clf = joblib.load("phishing.pkl")  # モデル読みこみ
     tokenizedUrl = proc.tokenize(url)  # トークン化
     normalizedUrl = proc.englishNormalization(tokenizedUrl)  # 正規化
+    print(w2iClass.convert(normalizedUrl))
+    return
     feature = proc.wordStr2IntVec(pd.Series(normalizedUrl))  # ベクトル化
-    print(feature)
     predictResult = clf.predict(feature)
     return predictResult
 
 
 if __name__ == "__main__":
-    print("good:", infer("facebook.com/people/Desiree-Gordon/507399416"))  # good
-    print("bad:", infer("dutchweb.gtphost.com/zimbra/exch/owa/uleth/index.html"))  # bad
+    w2i = wordStr2IntVec()
+    print("good:", infer("facebook.com/people/Desiree-Gordon/507399416", w2i))  # good
+    exit()
+    print(
+        "bad:", infer("dutchweb.gtphost.com/zimbra/exch/owa/uleth/index.html", w2i)
+    )  # bad
 
