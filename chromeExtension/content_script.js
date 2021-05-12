56947c859c5b19da0afa015e1af4b240bd9fd5ad
xryuseix <ryusei143.shootingstar@gmail.com>
2021-05-12 15:28:12 +0900

    feat: ページ上部にテキストを挿入

--- chromeExtension/content_script.js
+++ chromeExtension/content_script.js
@@ -1,4 +1,18 @@
-let Data = { Title: "", URL: "" }; // とりま格納する変数
+let Data = { Title: "サンプルタイトル", URL: "http://www.sample.com" };// あとで入れる
 
-const txtBox = document.querySelector("input");
-txtBox.value = "AAAAAAAAAAAAAAAAAAAAA";
+// prependChildを定義
+Element.prototype.prependChild = function(el){
+  this.insertBefore(el, this.firstChild)
+}
+
+var bodyElement = document.querySelector('body');
+var heavyTire = document.createElement('div');
+
+// ここからコンテンツを作る
+
+// heavyTire.className = 'heavy-tire';
+heavyTire.textContent = '追加テキスト';
+
+// ここまで
+
+bodyElement.prependChild(heavyTire);
