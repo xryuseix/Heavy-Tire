949136a9000c2637d51736072d6daa48442655b6
xryuseix <ryusei143.shootingstar@gmail.com>
2021-05-19 12:03:57 +0900

    feat: fetch phising api

--- chromeExtension/content_script.js
+++ chromeExtension/content_script.js
@@ -1,17 +1,37 @@
-let Data = { Title: "サンプルタイトル", URL: "http://www.sample.com" };// あとで入れる
+// ページ操作用スクリプト
+let Data = { Title: "サンプルタイトル", URL: "http://www.sample.com" }; // あとで入れる
 
 // prependChildを定義
-Element.prototype.prependChild = function(el){
-  this.insertBefore(el, this.firstChild)
-}
+Element.prototype.prependChild = function (el) {
+  this.insertBefore(el, this.firstChild);
+};
 
-var bodyElement = document.querySelector('body');
-var heavyTire = document.createElement('div');
+var bodyElement = document.querySelector("body");
+var heavyTire = document.createElement("div");
+heavyTire.className = "heavy-tire";
 
 // ここからコンテンツを作る
 
-// heavyTire.className = 'heavy-tire';
-heavyTire.textContent = '追加テキスト';
+fetch("https://heavy-tire.herokuapp.com/?url=twitter.com")
+  .then((response) => {
+    return response.json();
+  })
+  .then((result) => {
+    putResponseData(result);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+// APIから取得したデータを出力
+function putResponseData(jsonObj) {
+  // TODO: Errorの時の処理入れる
+  if ("result" in jsonObj) {
+    heavyTire.textContent = `{"result": ${jsonObj.result}}`;
+  }else{
+    console.log("[Heavy-Tire] ERROR: failed to fatch.")
+  }
+}
 
 // ここまで
 
