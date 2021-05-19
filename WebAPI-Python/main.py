2280fe52c5943ffd962386c6c71a09aacfcd7df1
xryuseix <ryusei143.shootingstar@gmail.com>
2021-05-19 11:46:10 +0900

    feat: CORS settings

--- WebAPI-Python/main.py
+++ WebAPI-Python/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import inferModel.infer as Infer
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root(url: str = None):
@@ -11,3 +19,6 @@ async def root(url: str = None):
         w2i = Infer.wordStr2IntVec()
         result = 0 if Infer.infer(url, w2i)[0]== "good" else 1
         return {"result": result}
+
+# uvicorn main:app --reload --workers 1 --host 0.0.0.0 --port 8080
+# http://localhost:8080
